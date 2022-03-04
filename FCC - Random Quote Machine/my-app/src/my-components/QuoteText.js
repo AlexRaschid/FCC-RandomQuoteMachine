8c9165b97f608467a1d535e0118677888eb355cd
Alexander Raschid <alexraschid1337@gmail.com>
2022-03-04 15:49:51 -0500

    reorganized quote layout and added new icons

--- FCC - Random Quote Machine/my-app/src/my-components/QuoteText.js
+++ FCC - Random Quote Machine/my-app/src/my-components/QuoteText.js
@@ -6,10 +6,16 @@ export default class QuoteText extends React.Component{
         super(props);
     }
 
+
     render(){
         return(
         <div className="row">
-            <p className="">Quote Text</p>    
+
+            <p id="text" className="">
+                <i className="fas fa-quote-left" />
+                     {` ${this.props.jokes[0]} `} 
+                <i className="fas fa-quote-right" />
+            </p>    
         </div>
         );
 
