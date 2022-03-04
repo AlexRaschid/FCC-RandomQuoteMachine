16daaa3bff6dfaf892b0e8b39c03e2032d536ed5
Alexander Raschid <alexraschid1337@gmail.com>
2022-03-04 10:28:44 -0500

    Re aligned Card box to be vertically center

--- FCC - Random Quote Machine/my-app/src/my-components/Card.js
+++ FCC - Random Quote Machine/my-app/src/my-components/Card.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import QuoteText from './QuoteText';
+import QuoteAuthor from './QuoteAuthor';
+import QuoteButtons from './QuoteButtons';
 
 export class Card extends React.Component{
     
@@ -16,34 +19,25 @@ export class Card extends React.Component{
             .then(response => response.json())
             .then(data => {
                 //Returns Jokes and returns all clean jokes (all filters = false)
-                console.log(data.jokes);
                 let cleanJokes = data.jokes.map(objNum => {
-                    //console.log(Object.values(objNum.flags));
-                    //console.log(objNum);
-                    //console.log(Object.values(objNum.flags));
-
                     //objNum.flags is an array of boolean values. I want the jokes
                     //that have ALL boolean values of false
                     let isCurrentJokeClean = Object.values(objNum.flags).every(value => {
-                        console.log("value: " + value);
                         return value === true ? false : true;
                     });
-                    
+
                     switch(isCurrentJokeClean){
                         case true:
                             return objNum.joke;
                     }
-
                     //filter(Boolean) gets rid of any falsy values ie undefined
                 }).filter(Boolean);
 
-                //console.log(cleanJokes);
                 
 
                 this.setState({
                     jokes: [...cleanJokes]
                 });
-                //console.log(this.state.jokes);
             });
             
     }
@@ -51,24 +45,15 @@ export class Card extends React.Component{
     
     render(){
         return(
-        <div className="container">
-           <div className="row">
-
-                
-                    <div className="card col-md-8 mx-auto">
+        <div className="container h-100">
+           <div className="d-flex flex-column min-vh-100 justify-content-center align-items-center">
+                    <div className="card col-md-8 mx-auto ">
                         <div className="card-body">
-                            <p>{console.log(this.state.jokes)}</p>
-                            <p>Author here</p>
+                            <QuoteText jokes={this.state.jokes}/>
+                            <QuoteAuthor />
+                            <QuoteButtons />
                         </div>
-                        <div className="row">
-                            <button type="button" className="col-2 btn btn-primary">Button 1</button>
-                            <button href="#" className="col-2 btn btn-primary">Button 2</button>
-                            <button href="#" className="col-2 btn btn-primary">Button 3</button>
-                        </div> 
-                        
                     </div>
-                
-            
           </div>
         </div>
         );
